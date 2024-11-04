3961de9846e7dd9aff6fce11894ec1beac559cad
jLuseno161 <joyluseno2@gmail.com>
2024-11-04 16:15:20 +0300

    update on user login and verification

--- cypress/e2e/search.cy.js
+++ cypress/e2e/search.cy.js
@@ -1,17 +1,22 @@
 describe('Land Search Process', () => {
-    it('page successfully loads', () => {
-        //Visit Ardhisasa Platform open login page
+    before(() => {
+        // Visit Ardhisasa Platform open login page
         cy.visit('/home')
         cy.contains('Ardhisasa')
-        cy.contains('Ardhisasa is an online platform that allows Citizens, stakeholders and interested parties')
         cy.xpath("//a[@routerlink='/account']").should('be.visible').click();
         cy.contains('Login')
 
         //Enter login credentials and verify user
-        cy.xpath("(//div[@class='mat-form-field-infix'])[1]").type("PA0G09FA0B")
-        cy.xpath("(//div[@class='mat-form-field-infix'])[2]").type('Test@123')
+        cy.get('input[formcontrolname="username"]').type("PA0G09FA0B")
+        cy.get('input[formcontrolname="password"]').type('Test@123')
 
         cy.xpath("//button[@type='submit']").should('be.visible').click();
-        cy.xpath("//button[@id='verify']").should('be.visible').click();
-    })
+        cy.get('button#verify').should('be.visible').click();
+
+        //load user profile
+        cy.intercept('http://192.168.214.184/acl/api/v1/accounts/userprofiledetails').as('getProfilePhoto');
+        cy.wait('@getProfilePhoto');
+
+        cy.get('.greetings').should('contain', 'Hi Monicah, welcome');
+    });
 })
